91aa0579d3043140f9505053886674e108c12a4e
Bruno Salmon <dev.salmonb@gmail.com>
2023-03-14 21:53:34 +0000

    Removed double token in modules names

---
+++ webfx-demo-tetris-application-gluon/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.tetris.application.gluon {
+
+    // Direct dependencies modules
+    requires webfx.demo.tetris.application;
+    requires webfx.kit.openjfx;
+    requires webfx.platform.audio.gluon;
+    requires webfx.platform.boot.java;
+    requires webfx.platform.console.java;
+    requires webfx.platform.os.gluon;
+    requires webfx.platform.resource.gluon;
+    requires webfx.platform.scheduler.java;
+    requires webfx.platform.shutdown.gluon;
+    requires webfx.platform.storage.java;
+    requires webfx.platform.storagelocation.gluon;
+    requires webfx.platform.useragent.gluon;
+    requires webfx.platform.visibility.gluon;
+
+    // Meta Resource package
+    opens dev.webfx.platform.meta.exe;
+
+}
---
+++ webfx-demo-tetris-application/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.tetris.application {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires webfx.extras.scalepane;
+    requires webfx.kit.util.scene;
+    requires webfx.platform.audio;
+    requires webfx.platform.resource;
+    requires webfx.platform.shutdown;
+    requires webfx.platform.storage;
+    requires webfx.platform.useragent;
+
+    // Exported packages
+    exports eu.hansolo.fx.tetris;
+
+    // Resources packages
+    opens eu.hansolo.fx.tetris;
+
+    // Provided services
+    provides javafx.application.Application with eu.hansolo.fx.tetris.Main;
+
+}
