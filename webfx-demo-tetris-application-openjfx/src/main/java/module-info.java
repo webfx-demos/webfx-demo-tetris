1b18683fd403a7502ec1d6cc35ebad00846a6513
Bruno Salmon <dev.salmonb@gmail.com>
2023-03-14 21:53:34 +0000

    Removed double token in modules names

---
+++ webfx-demo-tetris-application-openjfx/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.tetris.application.openjfx {
+
+    // Direct dependencies modules
+    requires webfx.demo.tetris.application;
+    requires webfx.kit.openjfx;
+    requires webfx.kit.platform.audio.openjfx.gwt;
+    requires webfx.kit.platform.visibility.openjfx;
+    requires webfx.platform.boot.java;
+    requires webfx.platform.console.java;
+    requires webfx.platform.os.java;
+    requires webfx.platform.resource.java;
+    requires webfx.platform.scheduler.java;
+    requires webfx.platform.shutdown.java;
+    requires webfx.platform.storage.java;
+    requires webfx.platform.storagelocation.java;
+    requires webfx.platform.useragent.java.client;
+
+    // Meta Resource package
+    opens dev.webfx.platform.meta.exe;
+
+}
